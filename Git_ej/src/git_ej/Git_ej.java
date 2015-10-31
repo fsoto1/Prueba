197797c9b7f081ef3b519035690229f0757c66ab
fsoto1 <f.soto@outlook.com>
2015-10-31 19:55:30 -0300

    Proyecto
    Agregado el proyecto de netbeans

---
+++ Git_ej/src/git_ej/Git_ej.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_ej;
+
+/**
+ *
+ * @author Franco
+ */
+public class Git_ej {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Inicio obj = new Inicio();
+        obj.show();
+        //obj.setVisible(true);
+        
+    }
+    
+}
